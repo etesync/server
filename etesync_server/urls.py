27d56ee87be942ec435df44857222ba1761c7d82
Victor Rezende dos Santos <victor.rezende@outlook.com>
2018-05-01 12:46:50 -0300

    Update Dependencies
    * Updated etesync/journal-manager to v0.5.2
    * Updated Django to 2.0.5
      - Updated documentation links
      - Changed syntax and imports of `urls.py`
    * Updated Django REST Framework to 3.8.2
    * Updated DFR Nested Routers to 0.90.2

--- etesync_server/urls.py
+++ etesync_server/urls.py
@@ -1,19 +1,19 @@
 """etesync_server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, path, re_path
 from django.contrib import admin
 
 from rest_framework_nested import routers
@@ -32,9 +32,9 @@ journals_router.register(r'entries', views.EntryViewSet, base_name='journal-entr
 
 
 urlpatterns = [
-    url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/', include(journals_router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', token_views.obtain_auth_token),
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^api/v1/', include(router.urls)),
+    re_path(r'^api/v1/', include(journals_router.urls)),
+    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^api-token-auth/', token_views.obtain_auth_token),
+    path('admin/', admin.site.urls),
 ]
