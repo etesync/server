8f50a69b39bef7f421590e3de26b822df0ceaf6e
Tom Hacohen <tom@stosb.com>
2018-11-01 11:51:26 +0000

    Add missing STATIC_ROOT.
    Thanks @jrvarma.

--- etesync_server/settings.py
+++ etesync_server/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 JOURNAL_API_PERMISSIONS = (
