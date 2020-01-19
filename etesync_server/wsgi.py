79ea7d1c5d6e2e538f4b89033e9d29d230980260
Robin Schneider <ypid@riseup.net>
2020-01-19 19:01:49 +0100

    Add support to host under a subpath like /random_psk_here/

--- etesync_server/wsgi.py
+++ etesync_server/wsgi.py
@@ -13,4 +13,20 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "etesync_server.settings")
 
-application = get_wsgi_application()
+_application = get_wsgi_application()
+
+
+def application(environ, start_response):
+    # https://stackoverflow.com/questions/47941075/host-django-on-subfolder/47945170#47945170
+    script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+    if script_name:
+        environ['SCRIPT_NAME'] = script_name
+        path_info = environ['PATH_INFO']
+        if path_info.startswith(script_name):
+            environ['PATH_INFO'] = path_info[len(script_name):]
+
+    scheme = environ.get('HTTP_X_SCHEME', '')
+    if scheme:
+        environ['wsgi.url_scheme'] = scheme
+
+    return _application(environ, start_response)
