4948e91c65db0842d9c4d50929549df9353a2b65
Tom Hacohen <tom@stosb.com>
2020-06-28 16:52:14 +0300

    django_etebase: make migration generic and not depend on myauth.

--- django_etebase/migrations/0002_userinfo.py
+++ django_etebase/migrations/0002_userinfo.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('myauth', '0001_initial'),
+        # XXX removed this to make this migration generic ('myauth', '0001_initial'),
         ('django_etebase', '0001_initial'),
     ]
 
