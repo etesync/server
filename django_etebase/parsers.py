0585d6ee927ad5aa4a4baea3dac786db1a49b647
Tom Hacohen <tom@stosb.com>
2020-12-15 13:58:59 +0200

    Chunk file uploader: accept all kinds of media types.
    This restriction was unnecessary and annoying to deal with in clients.

--- django_etebase/parsers.py
+++ django_etebase/parsers.py
@@ -6,8 +6,6 @@ class ChunkUploadParser(FileUploadParser):
     Parser for chunk upload data.
     """
 
-    media_type = "application/octet-stream"
-
     def get_filename(self, stream, media_type, parser_context):
         """
         Detects the uploaded file name.
