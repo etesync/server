10ff303b754f8147acdc4f1740d27bf3dbefa915
Tom Hacohen <tom@stosb.com>
2020-12-28 17:09:20 +0200

    Fix formatting.

--- etebase_fastapi/main.py
+++ etebase_fastapi/main.py
@@ -12,6 +12,7 @@ from .member import member_router
 from .invitation import invitation_incoming_router, invitation_outgoing_router
 from .msgpack import MsgpackResponse
 
+
 def create_application(prefix=""):
     app = FastAPI()
     VERSION = "v1"
@@ -21,8 +22,12 @@ def create_application(prefix=""):
     app.include_router(collection_router, prefix=f"{BASE_PATH}/collection", tags=["collection"])
     app.include_router(item_router, prefix=f"{BASE_PATH}/collection/{COLLECTION_UID_MARKER}", tags=["item"])
     app.include_router(member_router, prefix=f"{BASE_PATH}/collection/{COLLECTION_UID_MARKER}", tags=["member"])
-    app.include_router(invitation_incoming_router, prefix=f"{BASE_PATH}/invitation/incoming", tags=["incoming invitation"])
-    app.include_router(invitation_outgoing_router, prefix=f"{BASE_PATH}/invitation/outgoing", tags=["outgoing invitation"])
+    app.include_router(
+        invitation_incoming_router, prefix=f"{BASE_PATH}/invitation/incoming", tags=["incoming invitation"]
+    )
+    app.include_router(
+        invitation_outgoing_router, prefix=f"{BASE_PATH}/invitation/outgoing", tags=["outgoing invitation"]
+    )
 
     if settings.DEBUG:
         from etebase_fastapi.test_reset_view import test_reset_view_router
@@ -30,12 +35,16 @@ def create_application(prefix=""):
         app.include_router(test_reset_view_router, prefix=f"{BASE_PATH}/test/authentication")
 
     app.add_middleware(
-        CORSMiddleware, allow_origin_regex="https?://.*", allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+        CORSMiddleware,
+        allow_origin_regex="https?://.*",
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
     app.add_middleware(TrustedHostMiddleware, allowed_hosts=settings.ALLOWED_HOSTS)
-
 
     @app.exception_handler(CustomHttpException)
     async def custom_exception_handler(request: Request, exc: CustomHttpException):
         return MsgpackResponse(status_code=exc.status_code, content=exc.as_dict)
+
     return app
