22b93f81b614d53784c09318cef33b489b2e126a
Xiretza <xiretza@xiretza.xyz>
2022-05-08 15:16:54 +0200

    fix: use django.urls.path instead of deprecated django.conf.urls.url

--- etebase_server/urls.py
+++ etebase_server/urls.py
@@ -1,7 +1,6 @@
 import os
 
 from django.conf import settings
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import TemplateView
@@ -9,7 +8,7 @@ from django.views.static import serve
 from django.contrib.staticfiles import finders
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="success.html")),
 ]
 
